a6ed8da782635704680fa6b95803300ada4e8195
Diego Obando <diegoobando20@gmail.com>
2021-05-25 02:08:08 -0400

    more data from api

--- src/_data/news.js
+++ src/_data/news.js
@@ -1,12 +1,26 @@
 const axios = require("axios");
+const countries = require("./countries.json");
 require('dotenv').config();
 
-// get the data from api
-module.exports = async function() {
+async function getNews(country) {
     try {
-        const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.NEWS_API_KEY}&pageSize=5`);
-        return response.data;
+        const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=${country}&apiKey=${process.env.NEWS_API_KEY}&pageSize=5`);
+        return {
+            "country" : country,
+            "articles" : response.data.articles
+        }
     } catch (error) {
         console.error(error);
     }
 }
+
+// get the data from api
+module.exports = async function() {
+
+    var newPromises = countries.map(getNews);
+    return Promise.all(newPromises).then( newsObjects => {
+       console.log('newsObjects :', newsObjects);
+       return [].concat.apply([], newsObjects);
+    });
+
+};
